074a5ef2fcbf25f359e52da07026c93090954cbe
Ege <egecam000@gmail.com>
2023-09-07 15:22:45 +0300

    integrate spotify api

--- web/egecamdev/views.py
+++ web/egecamdev/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 # Create your views here.
 def index(req):
+    client_id = "5b1060dab6f44ac48ec834bf7b8b41f4"
+    client_secret = "d15a4f015d384430ab6930b5b50de279"
+    appname = "nowplaying"
+
+
+    # sp_oauth = SpotifyOAuth(client_id, client_secret, redirect_uri="http://localhost:8000/callback", scope="user-read-currently-playing")
+
+    # access_token = sp_oauth.get_access_token()  
+    # refresh_token = sp_oauth.get_refresh_token()  
+    
     return render(req, 'index.html')
+
+def callback(req):
+    return render(req, 'spotify.html')
 
 def projects(req):
     return render(req, 'projects.html')
